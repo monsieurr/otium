daf089b7a5009a3e335bd057910ccabea6519bc2
monsieurr <plouftoto@gmail.com>
2021-02-24 15:51:25 +0100

    Updates

---
+++ component---src-pages-404-js-3e430b64ac42b69a30e4.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),o=n("Bl7J"),c=n("vrFN"),r=n("Wbzz");t.default=function(){return l.a.createElement(o.a,null,l.a.createElement(c.a,{title:"404: Not found"}),l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"This is not the right place to be, come back home"),l.a.createElement(r.Link,{to:"/"},"Back to home"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-3e430b64ac42b69a30e4.js.map
